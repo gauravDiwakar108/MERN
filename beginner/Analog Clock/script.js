ec7c6bb2eb847da833afdd5519ff0b5cefa0d62c
Gaurav <gauravsubhash9999@gmail.com>
2026-04-25 17:33:58 +0530

    Analog Clock - Completed

--- beginner/Analog Clock/script.js
+++ beginner/Analog Clock/script.js
@@ -7,4 +7,12 @@ setInterval(() =>{
     let hRotation = 30*hTime + mTime/2;
     let mRotation = 6*mTime;
     let sRotation = 6*sTime;
+
+    let hour = document.getElementById("hour");
+    let minute = document.getElementById("minute");
+    let second = document.getElementById("second");
+
+    hour.style.transform = `rotate(${hRotation}deg)`;
+    minute.style.transform = `rotate(${mRotation}deg)`;
+    second.style.transform = `rotate(${sRotation}deg)`;
 }, 1000);
