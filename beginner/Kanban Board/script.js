bd3af02345576794f4f60d777ae4a55f3337bc64
Gaurav <gauravsubhash9999@gmail.com>
2026-08-21 12:33:47 +0530

    draggable, task added

--- beginner/Kanban Board/script.js
+++ beginner/Kanban Board/script.js
@@ -35,7 +35,43 @@ const addDragEventsOnColumn = (column) => {
 };
 
 addDragEventsOnColumn(todo);
-
 addDragEventsOnColumn(progress);
-
 addDragEventsOnColumn(done);
+
+
+// modal related logic
+
+const toggleModalButton = document.querySelector("#toggle-modal");
+const modalBg = document.querySelector(".modal .bg");
+const modal = document.querySelector(".modal");
+const addTaskButton = document.querySelector("#add-new-task");
+
+toggleModalButton.addEventListener("click", () => {
+    modal.classList.toggle("active");
+});
+
+modalBg.addEventListener("click", () => {
+    modal.classList.remove("active");
+});
+
+addTaskButton.addEventListener("click", () => {
+    const taskTitle = document.querySelector("#task-title-input").value;
+    const taskDesc = document.querySelector("#task-desc-input").value;
+
+    const div = document.createElement("div");
+
+    div.classList.add("task");
+    div.setAttribute("draggable", "true");
+
+    div.innerHTML = `
+    <h2>${taskTitle}</h2>
+    <p>${taskDesc}</p>
+    <button>Delete</button>
+    `
+    todo.appendChild(div);
+
+    div.addEventListener("drag", (e)=>{
+        dragElement = div;
+    })
+    modal.classList.remove("active");
+});
