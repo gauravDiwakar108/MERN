73f05f4fdca3e13f57b026c684ff31d58b5cecc1
Gaurav <gauravsubhash9999@gmail.com>
2026-08-23 14:12:45 +0530

    completed serviceCard component

--- intermediate/agency.ai/frontend/src/Components/ServiceCard.jsx
+++ intermediate/agency.ai/frontend/src/Components/ServiceCard.jsx
@@ -7,19 +7,19 @@ const ServiceCard = ({ service, index }) => {
 
     const divRef = useRef(null);
 
-    const handleMouseMove = (e) =>{
+    const handleMouseMove = (e) => {
         const bounds = divRef.current.getBoundingClientRect();
-        setPosition({x: e.clientX - bounds.left, y: e.clientY - bounds.top})
+        setPosition({ x: e.clientX - bounds.left, y: e.clientY - bounds.top })
     };
 
     return (
         <div className='relative overflow-hidden max-w-lg m-2 sm:m-4 rounded-xl border border-gray-200 dark:border-gray-700 shadow-2xl shadow-gray-100 dark:shadow-white/10'
-            onMouseEnter={()=>setVisible(true)}
-            onMouseLeave={()=>setVisible(false)}
+            onMouseEnter={() => setVisible(true)}
+            onMouseLeave={() => setVisible(false)}
             ref={divRef}
             onMouseMove={handleMouseMove}>
 
-            <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-[300px] h-[300px] absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`} style={{ top: position.y - 150, left: position.x - 150 }} />
+            <div className={`pointer-events-none blur-2xl rounded-full bg-gradient-to-r from-blue-500 via-indigo-500 to-purple-500 w-75 h-75 absolute z-0 transition-opacity duration-500 mix-blend-lighten ${visible ? 'opacity-70' : 'opacity-0'}`} style={{ top: position.y - 150, left: position.x - 150 }} />
 
             <div className='flex items-center gap-10 p-8 hover:p-7.5 hover:m-0.5 transition-all rounded [10px] bg-white dark:bg-gray-900 z-10 relative'>
 
