b67bfbb7d3b94f1aaf658f3703b52f1314952d3c
Gaurav <gauravsubhash9999@gmail.com>
2026-09-09 15:07:30 +0530

    motion added to services section

--- intermediate/agency.ai/frontend/src/Components/Services.jsx
+++ intermediate/agency.ai/frontend/src/Components/Services.jsx
@@ -1,6 +1,7 @@
 import assets from '../assets/assets'
 import ServiceCard from './ServiceCard'
 import Title from './Title'
+import { motion } from 'motion/react'
 
 const Services = () => {
 
@@ -28,7 +29,13 @@ const Services = () => {
     ]
 
     return (
-        <div className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white' id='services'>
+        <motion.div
+            initial="hidden"
+            whileInView="visible"
+            viewport={{ once: true }}
+            transition={{ staggerChildren: 0.2 }}
+
+            className='relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white' id='services'>
             <img className='absolute -top-110 -left-70 -z-1 dark:hidden' src={assets.bgImage2} alt="" />
 
             <Title
@@ -41,7 +48,7 @@ const Services = () => {
                     <ServiceCard key={index} service={service} index={index} />
                 ))}
             </div>
-        </div>
+        </motion.div>
     )
 }
 
