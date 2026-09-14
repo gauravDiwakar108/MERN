c3ebd0b9283c3e89b60ba00f8eb913beb75b650d
Gaurav <gauravsubhash9999@gmail.com>
2026-09-14 13:55:12 +0530

    featured section component completed

--- advance/react/Car Rental/client/src/components/FeaturedSection.jsx
+++ advance/react/Car Rental/client/src/components/FeaturedSection.jsx
@@ -15,7 +15,7 @@ const FeaturedSection = () => {
 
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-18">
                 {
-                    dummyCarData.slice(0, 6).map((car) => (
+                    dummyCarData.slice(0, 3).map((car) => (
                         <div key={car._id}>
                             <CarCard car={car} />
                         </div>
